4cb057fb8294e6053c9432d4d5147a320387c4dd
Bhushan-Alva <69306298+Bhushan-Alva@users.noreply.github.com>
2023-10-06 13:59:59 +0530

    Update Dummydata.sql

--- Dummydata.sql
+++ Dummydata.sql
@@ -1,43 +1,215 @@
--- Insert data into reservation_status table
-INSERT INTO reservation_status (id, status_value) VALUES (1, 'Active');
-INSERT INTO reservation_status (id, status_value) VALUES (2, 'Cancelled');
+1.
+CREATE or REPLACE procedure payment_details(
+	rent_id number) is
+	type rec_type is record
+	(c_id customers.customer_id%type,
+	c_name customers.customer_name%type,
+	r_pay rent_payment.payment%type,
+	m_pay mod_pay.mod_payment%type,
+	total number);
+	type rec is  table of rec_type ;
+	payment rec;
+BEGIN
+	SELECT  c.customer_id, c.customer_name,
+	r.payment, m.mod_payment, (r.payment+m.mod_payment) as total
+	bulk collect into payment
+	FROM rent_payment r
+	JOIN customers c on(r.customer_id=c.customer_id)
+	JOIN mod_pay m on (r.mod_id=m.mod_id)
+	WHERE rental_id=rent_id;
+	for i in payment.first..payment.last loop
+	dbms_output.put_line('Customer id: ' ||  payment(i).c_id || '
+' || 'Customer name: ' ||  payment(i).c_name || '
+' || 'Rent Amount: ' ||  payment(i).r_pay || '
+' || 'Fine: ' ||  payment(i).m_pay || '
+' || 'Total amount: ' ||  payment(i).total);
+	END loop;
+EXCEPTION
+	when others then
+        dbms_output.put_line('No record found');
+END payment_details;
+/
 
--- Insert data into category table
-INSERT INTO category (id, category_name) VALUES (1, 'Fiction');
-INSERT INTO category (id, category_name) VALUES (2, 'Non-Fiction');
+************************************************************************
+2.
+create or replace function get_receipt (
+cust_id number) return varchar2 is
+cursor c1 is select rp.rental_id as r_id, c.customer_id as c_id, c.customer_name as c_name, rp.payment as r_pay, mp.mod_payment as m_pay, (rp.payment+mp.mod_payment) as total, rent_srt_date as rsd, rent_ret_date as rrd
+	from rent_payment rp
+	join rentals r on(r.rental_id=rp.rental_id)
+	join customers c on(rp.customer_id=c.customer_id)
+	join mod_pay mp on (rp.mod_id=mp.mod_id)
+	where c.customer_id=cust_id;
+v_rec c1%rowtype;
+receipt varchar2(30000);
+begin
+open c1;
+loop
+fetch c1 into v_rec;
+exit when c1%notfound;
+receipt := 'Rental id: ' || v_rec.r_id || '
+' || 'Customer id: ' || v_rec.c_id || '
+' || 'Customer name: ' || v_rec.c_name || '
+' || 'Rent amount: ' || v_rec.r_pay || '
+' || 'extras: ' || v_rec.m_pay || '
+' || 'Total amount: ' || v_rec.total || '
+' || 'Rent start date: ' || v_rec.rsd || '
+' || 'Rent return date: ' || v_rec.rrd || '
+*******************
+';
+end loop;
+close c1;
+return receipt;
+end get_receipt;
+/
+**************************************************************************
 
--- Insert data into book table
-INSERT INTO book (id, title, category_id, publication_date, copies_owned) VALUES (1, 'Sample Fiction Book', 1, TO_DATE('2023-01-01', 'YYYY-MM-DD'), 100);
-INSERT INTO book (id, title, category_id, publication_date, copies_owned) VALUES (2, 'Sample Non-Fiction Book', 2, TO_DATE('2023-02-01', 'YYYY-MM-DD'), 150);
+4.
+DECLARE
+	y number := &year;
+	m_cust_id number;
+BEGIN
+	SELECT customer_id into m_cust_id 
+	FROM (select customer_id, to_char(rent_srt_date, 'yyyy') as year, count(customer_id) 
+	FROM rentals  group by customer_id, to_char(rent_srt_date, 'yyyy') 
+	ORDER BY count(customer_id) desc, to_char(rent_srt_date, 'yyyy')) 
+	WHERE year=y and rownum=1;
+	DECLARE
+		CURSOR c1 is 
+		SELECT c.customer_id as c_id, c.customer_name as c_name, r.rental_id as r_id, r.rent_srt_date as rsd 
+		FROM customers c JOIN rentals r on(c.customer_id=r.customer_id) 
+		WHERE c.customer_id=m_cust_id and to_char(r.rent_srt_date, 'yyyy')=y;
+		v_rec c1%rowtype;
+	BEGIN
+		open c1;
+		loop
+		fetch c1 into v_rec;
+		exit when c1%notfound;
+		dbms_output.put_line(v_rec.c_id || ' ' || v_rec.r_id || ' ' ||v_rec.c_name || ' ' ||v_rec.rsd);
+		end loop;
+		close c1;
+	END;
+EXCEPTION
+	when no_data_found then
+	dbms_output.put_line('No record found');
+END;
+/
 
--- Insert data into author table
-INSERT INTO author (id, first_name, last_name) VALUES (1, 'John', 'Doe');
-INSERT INTO author (id, first_name, last_name) VALUES (2, 'Jane', 'Smith');
+***********************************************************************
+5.
+CREATE or REPLACE procedure month_rentals (
+	p_agency varchar2,
+	p_date varchar2) is
+	type rec_type is record (
+		r_id rent_payment.rental_id%type,
+		c_id customers.customer_id%type,
+		c_name customers.customer_name%type,
+		a_id agency.agency_id%type,
+		rsd date);
+	type result_tab is table of rec_type;
+	v_rec result_tab;
+BEGIN
+	SELECT r.rental_id, r.customer_id, c.customer_name, r.agency_id, r.rent_srt_date BULK COLLECT into v_rec
+	FROM rentals r
+	JOIN customers c on (r.customer_id=c.customer_id)
+	JOIN agency a on(c.agency_id=a.agency_id)
+	WHERE a.agency_loc=p_agency and to_char(rent_srt_date, 'mon-yyyy')=p_date;
+	for i in v_rec.first..v_rec.last loop
+	dbms_output.put_line(
+'Rental id: ' || v_rec(i).r_id || '
+' || 'Customer id: ' || v_rec(i).c_id || '
+' || 'Customer name: ' || v_rec(i).c_name || '
+' || 'Agency id: ' || v_rec(i).a_id || '
+' || 'Rent start date: ' || v_rec(i).rsd || '
+*******
+');
+	end loop;
+END month_rentals;
+/
 
--- Insert data into book_author table
-INSERT INTO book_author (book_id, author_id) VALUES (1, 1);
-INSERT INTO book_author (book_id, author_id) VALUES (2, 2);
+*************************************************************************
+3.
+DECLARE
+	y number;
+	type rec_type is record (
+		a_loc agency.agency_loc%type,
+		c_name varchar(20),
+		ct number);
+	type rec is  table of rec_type;
+	v_rec rec;
+BEGIN
+	SELECT a.agency_loc,to_char(rent_srt_date, 'yyyy') as year, 
+	count(a.agency_id) as count bulk collect into v_rec 
+	FROM agency a JOIN rentals r on(a.agency_id=r.agency_id) 
+	WHERE to_char(rent_srt_date, 'yyyy')=&y 
+	GROUP BY (to_char(rent_srt_date, 'yyyy'), 
+	a.agency_loc);
+	for i in v_rec.first..v_rec.last loop
+	dbms_output.put_line(v_rec(i).a_loc || ': ' || v_rec(i).ct);
+	end loop;
+END;
+/
 
--- Insert data into member_status table
-INSERT INTO member_status (id, status_value) VALUES (1, 'Active');
-INSERT INTO member_status (id, status_value) VALUES (2, 'Inactive');
+*************************************************************************
+6. with modification detals table (track_table)
 
--- Insert data into member table
-INSERT INTO member (id, first_name, last_name, joined_date, active_status_id) VALUES (1, 'Alice', 'Johnson', TO_DATE('2022-01-15', 'YYYY-MM-DD'), 1);
-INSERT INTO member (id, first_name, last_name, joined_date, active_status_id) VALUES (2, 'Bob', 'Smith', TO_DATE('2021-11-20', 'YYYY-MM-DD'), 2);
+*********
+create table track_table(sno number, customer_id number, old_phone_num number, new_phone_num number, status varchar2(30), mod_date date);
+*********
+create sequence s1;
+*********
+CREATE or REPLACE procedure update_track_table(o_pno number, n_pno number, c_id number)  is
+	pragma autonomous_transaction ;
+BEGIN
+	insert into track_table values ( s1.nextval, c_id, o_pno, n_pno, 'Data updated', sysdate);
+	commit;
+END update_track_table;
+/
 
--- Insert data into reservation table
-INSERT INTO reservation (id, book_id, member_id, reservation_date, reservation_status_id) VALUES (1, 1, 1, TO_DATE('2023-03-10', 'YYYY-MM-DD'), 1);
-INSERT INTO reservation (id, book_id, member_id, reservation_date, reservation_status_id) VALUES (2, 2, 2, TO_DATE('2023-03-12', 'YYYY-MM-DD'), 1);
 
--- Insert data into fine_payment table
-INSERT INTO fine_payment (id, member_id, payment_date, payment_amount) VALUES (1, 1, TO_DATE('2023-03-15', 'YYYY-MM-DD'), 10);
-INSERT INTO fine_payment (id, member_id, payment_date, payment_amount) VALUES (2, 2, TO_DATE('2023-03-16', 'YYYY-MM-DD'), 15);
+********
 
--- Insert data into loan table
-INSERT INTO loan (id, book_id, member_id, loan_date, returned_date) VALUES (1, 1, 1, TO_DATE('2023-02-20', 'YYYY-MM-DD'), TO_DATE('2023-03-05', 'YYYY-MM-DD'));
-INSERT INTO loan (id, book_id, member_id, loan_date, returned_date) VALUES (2, 2, 2, TO_DATE('2023-01-25', 'YYYY-MM-DD'), TO_DATE('2023-02-10', 'YYYY-MM-DD'));
+CREATE or REPLACE trigger check_num
+	before insert or update on customers for each row
+DECLARE
+	old_pno number;
+	new_pno number;
+	c_id number;
 
--- Insert data into fine table
-INSERT INTO fine (id, book_id, loan_id, fine_date, fine_amount) VALUES (1, 1, 1, TO_DATE('2023-03-08', 'YYYY-MM-DD'), 5);
-INSERT INTO fine (id, book_id, loan_id, fine_date, fine_amount) VALUES (2, 2, 2, TO_DATE('2023-02-28', 'YYYY-MM-DD'), 8);
+BEGIN
+	if (length(:new.customer_number) != 10) then
+		raise_application_error(-20555, 'THe phone number should contain 10 digits');
+	else
+		old_pno:=:old.customer_number;
+		new_pno:=:new.customer_number;
+		c_id:=:new.customer_id;
+		update_track_table(old_pno, new_pno, c_id);
+	end if;
+END check_num;
+/
+
+
+********************************************************************
+
+SELECT a.agency_loc,to_char(rent_srt_date, 'yyyy') as year, count(a.agency_id) 
+FROM agency a 
+JOIN rentals r on(a.agency_id=r.agency_id) 
+GROUP BY rollup(to_char(rent_srt_date, 'yyyy'), a.agency_loc);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
